c995473f7da0df5e02667c86928b8ec7c20b75b7
milaamaliadamayanti <amalia.damayanti.1805336@students.um.ac.id>
2021-02-10 16:19:25 +0700

    edit new

--- html/index.php
+++ html/index.php
@@ -7,36 +7,111 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
 
     <title>HALAMAN HOME</title>
   </head>
   <body>
-    
+    <br>
     <div class="container">
     	<div class="row">
-    		<div class="col" Style="border:solid 1px">
-    			<h2>Lorem ipsum</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    			<div>
-    				<a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
-					<a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>
+    		<div class="col-6" Style="">
+    			<h1>1 on 1 Tutoring</h1>
+    			<p>Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.</p>
+    			<div class="row">
+    				<div class="col-3" Style="">
+    					<a href="#" class="btn btn-primary rounded-pill btn-lg active" role="button" aria-pressed="true">LOG-IN</a>
+    				</div>
+    				<div class="col-3" Style="">
+    					<a href="#" class="btn btn-secondary rounded-pill btn-lg active" role="button" aria-pressed="true">SIGN-IN</a>
+    				</div>
     			</div>
     		</div>
-    		<div class="col" Style="border:solid 1px"><img src="img/blue.png" class="img-fluid" alt=""></div>
-    		<div class="w-100" Style="border:solid 1px"></div>
-    		<div class="col" Style="border:solid 1px">	<p>paragraf kata bytes daftar</p></div>
-    		<div class="col" Style="border:solid 1px"><h2>Lorem ipsum</h2>
-    			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+    		<div class="col-6" Style="">
+    			<img src="img/1.jpg" class="img-fluid" alt="">
     		</div>
-  		</div>
-	</div>
-	
+
+    		<div class="row">
+    			<div class="col-6" Style="">
+    				<h1>Our Process & Philosophy</h1>
+    				<h4><i class="fas fa-check-circle" style="color:blue"></i> normal</h4>
+    				<h4><i class="fas fa-check-circle" style="color:blue"></i> Good</h4>
+    				<h4><i class="fas fa-check-circle" style="color:blue"></i> Very Good</h4>
+    				<h4><i class="fas fa-check-circle" style="color:blue"></i> Expert</h4>
+    			</div>
+    			<div class="col-6" Style="">
+    				<h1>I Can't Belive How Fast my Test Score Improved!</h1>
+    				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+    				<div class="row">
+     					<div class="col-4" Style="">
+    						<a href="#" class="btn btn-primary rounded-pill btn-lg active" role="button" aria-pressed="true">GO CHECK</a></div>
+    				</div>
+    			</div>
+    		</div>
+    		<div><br></div>
+    		<div class="row">
+    			<div class="col-12 bg-info" Style="border-radius:30px">
+    				<br>
+    				<h1>Schedule a Free Consultan Today!</h1>
+    				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+     					<div class="col" align="center" Style="">
+    						<a href="#" class="btn btn-primary rounded-pill btn-lg active" role="button" aria-pressed="true">WATCH OUT</a></div>
+    				<br>
+    			</div>
+    		</div>
+    		<div><br></div>
+    		<div class="row row-cols-3">
+
+  				<div class="col" align="center" style="">
+  					<img src="img/3.jpg" class="img-thumbnail rounded-circle" width="200px">
+  					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+  				</div>
+  				<div class="col" align="center" style="">
+  					<img src="img/3.jpg" class="img-thumbnail rounded-circle" width="200px">
+  					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+  				</div>
+  				<div class="col" align="center" style="">
+  					<img src="img/3.jpg" class="img-thumbnail rounded-circle" width="200px">
+  					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+  				</div>
+  				<div><br></div>
+  			</div>
+
+  			<div class="row" align="center" style="">
+  				<div class="col">
+  					<h1>Let's Learn Together!</h1>
+  					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+  				</div>
+  				<div class="row">
+     					<div class="col" align="center" Style="">
+    						<a href="#" class="btn btn-primary rounded-pill btn-lg active" role="button" aria-pressed="true">START</a></div>
+    				</div>
+  			</div>
+
+  			<div class="row row-cols-2" align="center">
+  				<div class="col" style="">
+  					<br><br><br>
+  					<h4><i class="fas fa-mobile"></i></h4>
+  					<h4>Call</h4>
+  				</div>
+  				<div class="col" style="">
+  					<br><br><br>
+  					<h4><i class="fas fa-envelope-open"></i></h4>
+  					<h4>Message</h4>
+  				</div>
+ 			 </div>
+ 			 <div class="footer">
+ 			 	<img src="img/5.jpg" class="position:absolute" width="100%">
+ 			 </div>
+    	</div>
+
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
