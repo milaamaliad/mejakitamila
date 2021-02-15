f05ab410a47b244aaf7b531127b85d8967ab4a67
milaamaliadamayanti <amalia.damayanti.1805336@students.um.ac.id>
2021-02-15 16:34:16 +0700

    Update index.php

--- html/index.php
+++ html/index.php
@@ -8,12 +8,22 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@400;600;700&display=swap" rel="stylesheet">
 	<style>
 	 	.color {
-			border-left:blue 8px;
+			
 			padding-left:10px;
+			border-style: solid;
+			border-width: 7px;
+			border-top-color: white;
+			border-right-color: white;
+			border-bottom-color: white;
+			border-left-color: orange;
+		}
+
+		.font {
+			font-family: 'Bodoni Moda', serif;
 		}
 	</style>
 
@@ -21,10 +31,10 @@
   </head>
   <body>
     <br>
-    <div class="container">
+    <div class="container font">
     	<div class="row">
     		<div class="col-6" Style="">
-    			<h1>1 on 1 Tutoring</h1>
+    			<h1 class="font">1 on 1 Tutoring</h1>
     			<p>Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang </p>
     			<div class="row">
     				<div class="col-3" Style="">
@@ -39,9 +49,9 @@
     			<img src="img/1.jpg" class="img-fluid" alt="">
     		</div>
 
-    		<div class="row">
+    		<div class="row font">
     			<div class="col-6">
-    				<h1>Our Process & Philosophy</h1>
+    				<h1 class="font">Our Process & Philosophy</h1>
     				<h4><i class="fas fa-check-circle" style="color:blue"></i> normal</h4>
     				<h4><i class="fas fa-check-circle" style="color:blue"></i> Good</h4>
     				<h4><i class="fas fa-check-circle" style="color:blue"></i> Very Good</h4>
@@ -60,10 +70,16 @@
     		<div class="row">
     			<div class="col-12 bg-info" Style="border-radius:30px">
     				<br>
-    				<h1>Schedule a Free Consultan Today!</h1>
-    				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, .</p>
-     					<div class="col" align="center" Style="">
-    						<button class="btn btn-primary rounded-pill" type="submit">Go Check</button></div>
+
+    				<h1 class="font">Schedule a Free Consultan Today!</h1>
+    				<div class="row">
+    				<div class="col-8"><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, nde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p></div>
+    				<div class="col-4">
+    					<button class="btn btn-primary rounded-pill" type="submit">Go Check</button>
+    				</div>
+    				</div>
+    				
+     					
     				<br>
     			</div>
     		</div>
@@ -87,7 +103,7 @@
 
   			<div class="row" align="center" style="">
   				<div class="col">
-  					<h1>Let's Learn Together!</h1>
+  					<h1 class="font">Let's Learn Together!</h1>
   					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
   				</div>
   				<div class="row">
